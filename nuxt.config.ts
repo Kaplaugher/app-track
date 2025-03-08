2480d2e95eecf6289d9ef6e6c1d268119b2bafeb
kyle <kaplaugher94@gmail.com>
2025-03-08 17:09:36 -0500

    scrape/gen working. need linkedin fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,10 @@ export default defineNuxtConfig({
   },
 
   css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    apifyToken: process.env.APIFY_TOKEN,
+    geminiApiKey: process.env.GEMINI_API_KEY
+  },
 
   routeRules: {
     '/api/**': {
